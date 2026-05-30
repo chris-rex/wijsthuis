3e4df821c4298091f95fddc5a1fe05f53e610462
chris-rex <yinckx@gmail.com>
2026-05-30 16:19:28 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const CACHE = 'hoofdrekenen-v1';
+const FILES = [
+  '/wijsthuis/hoofdrekenen.html',
+  '/wijsthuis/apple-touch-icon.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      return cached || fetch(e.request).catch(() => caches.match('/wijsthuis/hoofdrekenen.html'));
+    })
+  );
+});
